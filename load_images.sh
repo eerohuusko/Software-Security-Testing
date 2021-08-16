53a35a2fb72afdae80ce9d7922e544d52500e55e
Kavyashree Shah <40597017+kavyashreeshah@users.noreply.github.com>
2021-08-16 21:00:18 +0530

    Code Bundle Updated

---
+++ load_images.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker load -i Common\ Web\ Vulnerabilities\ Lab/common_attacks.tar
+docker load -i Session\ Puzzling\ Lab/session_puzzling.tar
+docker load -i Padding\ Oracle\ Lab/oracle_padding.tar
+docker load -i LDAP\ Injection\ Lab/ldap-injection.tar
+docker load -i Cross\ Origin\ Resource\ Sharing\ Lab/cors.tar
+docker load -i URL\ Redirect\ Lab/url_redirect.tar
